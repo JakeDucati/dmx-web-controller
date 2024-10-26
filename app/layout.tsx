720ca158eaaf5ee05c0ab3a7d475c8237b0730de
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2024-10-25 19:28:53 -0700

    edit
    - fixed temporary error from fixtures component

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
                 <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
                     <div className="relative flex flex-col h-screen">
                         <NavHeader />
-                        <Fixtures />
+                        {/* <Fixtures /> */}
                         <Functions />
                         <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
                             <ToastContainer closeOnClick theme='dark' autoClose={2400} closeButton={false} />
--- components/fixtures.tsx
+++ components/fixtures.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "react-pro-sidebar";
 export default function Fixtures() {
     return (
         <Sidebar>
-            
+            <div></div>
         </Sidebar>
     );
 }
